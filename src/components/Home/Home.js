1d9898987a16a1cb47ebf68485f2f85eac20c1af
MariuszGawron <mariusz.gawron1@gmail.com>
2024-07-15 19:28:07 +0200

    Zaczątek headera

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,11 @@
 import React from "react";
 import HomeHeader from "./HomeHeader";
-import HomeThreeColumns from "./HomeThreeColumns";
-import HomeMainButtons from "./HomeMainButtons";
 import "../../scss/Home.scss";
 
 const Home = () => {
   return (
-    <div>
+    <div className="home-main">
       <HomeHeader />
-      <HomeThreeColumns />
-      <HomeMainButtons />
       {/* Dodaj inne komponenty sekcji/grup w razie potrzeby */}
     </div>
   );
--- src/components/Home/HomeHeader.js
+++ src/components/Home/HomeHeader.js
@@ -1,39 +1,58 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../../scss/HomeHeader.scss";
+import HomeThreeColumns from "./HomeThreeColumns";
+import HomeMainButtons from "./HomeMainButtons";
 
 const HomeHeader = () => {
   return (
     <header className="home-header">
-      <nav className="home-nav">
-        <ul className="home-nav-list">
-          <li>
-            <Link to="/start">Start</Link>
-          </li>
-          <li>
-            <Link to="/about">O co chodzi?</Link>
-          </li>
-          <li>
-            <Link to="/us">O nas</Link>
-          </li>
-          <li>
-            <Link to="/organizations">Fundacja i organizacje</Link>
-          </li>
-          <li>
-            <Link to="/contact">Kontakt</Link>
-          </li>
-        </ul>
-        <ul className="home-nav-auth">
-          <li>
-            <Link to="/login">Zaloguj się</Link>
-          </li>
-          <li>
-            <Link to="/register" className="btn">
-              Załóż konto
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <div className="home-img"></div>
+      <div className="home-header-nav">
+        <nav className="home-nav">
+          <ul className="home-nav-auth">
+            <li>
+              <Link to="/login" className="btn">
+                Zaloguj się
+              </Link>
+            </li>
+            <li>
+              <Link to="/register" className="btn">
+                Załóż konto
+              </Link>
+            </li>
+          </ul>
+          <ul className="home-nav-list">
+            <li>
+              <Link to="/start" className="btn">
+                Start
+              </Link>
+            </li>
+            <li>
+              <Link to="/about" className="btn">
+                O co chodzi?
+              </Link>
+            </li>
+            <li>
+              <Link to="/us" className="btn">
+                O nas
+              </Link>
+            </li>
+            <li>
+              <Link to="/organizations" className="btn">
+                Fundacja i organizacje
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact" className="btn">
+                Kontakt
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <HomeThreeColumns />
+        <HomeMainButtons />
+      </div>
     </header>
   );
 };
